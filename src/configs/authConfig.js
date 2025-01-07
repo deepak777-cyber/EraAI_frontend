967512d71d09802f41a1d783d8085313faf3c927
Deepak <deepak.khatri@excavateresearch.com>
2025-01-07 13:28:44 +0530

    update

--- src/configs/authConfig.js
+++ src/configs/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: "c2d4f853-7af1-4d19-9922-e3dfee228797", // Replace with your Azure AD app's client ID
       authority: "https://login.microsoftonline.com/ef086672-9048-432d-8a11-4c95bf2fef52", // Replace with your Azure AD tenant ID
-      redirectUri: window.location.origin, 
+      redirectUri: "http://192.168.1.98:900/", 
       postLogoutRedirectUri: "/", // Ensure this is the correct URI after logout
   
     },
